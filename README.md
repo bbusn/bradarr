c68d303b01ea1b9faeefbaddf89e8df4ce9bf2f3
bbusn <benoit.busnardo@gmail.com>
2024-11-13 13:00:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bradarr
+Jellyseerr movie requests manager
+-> Requires Jellyseerr, Jellyfin, one or more download plugins, vpn (unecessary)
---
+++ database/database.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: './database/database.sqlite',
+        logging: false,
+});
+
+sequelize.authenticate()
+        .then(() => console.log('Database connected...'))
+        .catch(err => console.log('Error: ' + err));
+
+module.exports = sequelize;
